55343aa185e35d0f275f741aac715118f4fb7f0e
PauloLemgruberJeunon <paulopljsousa@gmail.com>
2018-01-12 01:35:55 -0200

    content added

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import nltk
 import numpy as np
 from nltk.tag.stanford import CoreNLPPOSTagger
+# import matplotlib.pyplot as plt
+import heapq
 
 raw = nltk.corpus.gutenberg.raw('melville-moby_dick.txt')
-raw = raw.lower()
+raw = raw.lower() #Makes the text
 
 tokens = nltk.tokenize.RegexpTokenizer(r'\w+').tokenize(raw)
 tokens = tokens[30:6000]
@@ -61,8 +63,26 @@ for window in windows:
     window_verbs.clear()
     window_nouns.clear()
 
-print(windows)
+# plt.quiver(<list of x coordinates of origin>,<list of y coordinates of origin>,
+#            <list of x coordinates of the end>,<list of y coordinates of the end>, color='<alguma cor>')
 
-print(verb_columns)
-print(noun_rows)
-print(cooc_matrix)
+rows_cooc = np.zeros(noun_rows_size)
+for i in range(0, noun_rows_size):
+    rows_cooc[i] = np.sum(cooc_matrix[i])
+
+thirty_percent = int(np.ceil(noun_rows_size*0.3))
+thirty_percent_bigger_ind = heapq.nlargest(thirty_percent, range(noun_rows_size), rows_cooc.take)
+
+chopped_cooc_matrix = np.empty((thirty_percent,verb_columns_size))
+
+for i in range(0, thirty_percent):
+    chopped_cooc_matrix[i] = cooc_matrix[thirty_percent_bigger_ind[i]].copy()
+
+i = 0
+temp_dict = dict(zip(noun_rows.values(), noun_rows.keys()))
+noun_rows.clear()
+while i < thirty_percent:
+    noun_rows[temp_dict[thirty_percent_bigger_ind[i]]] = i
+    i+=1
+
+
